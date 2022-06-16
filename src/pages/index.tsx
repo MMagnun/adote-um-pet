b83cb2a4e1281ed482fbf6dd5ce0a88116c20e44
MMagnun <magnun.mmc@gmail.com>
2022-06-15 22:58:24 -0300

    Atualização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
-import type { NextPage } from 'next'
-import Titulo from '../ui/components/Titulo/Titulo'
-import Lista from '../ui/components/Lista/Lista'
+import type { NextPage } from 'next';
+import Titulo from '../ui/components/Titulo/Titulo';
+import Lista from '../ui/components/Lista/Lista';
+import { Dialog , TextField, Grid, DialogActions, Button, Snackbar } from '@mui/material';
+import { useIndex } from '../data/hooks/pages/useIndex';
 
 const Home: NextPage = () => {
+  const {
+    listaPets, 
+    petSelecionado, 
+    setPetSelecionado, 
+    email, 
+    setEmail, 
+    valor, 
+    setValor,
+    mensagem, 
+    setMensagem, 
+    adotar
+  } = useIndex();
+
   return (
     <div>
       <Titulo 
-      titulo="" 
-      subtitulo={
+       titulo="" 
+       subtitulo={
         <span>
           Com um pequeno valor mensal, você <br />
           pode <strong> adotar um pet virtualmente!</strong>
@@ -15,35 +30,57 @@ const Home: NextPage = () => {
       } />
 
       <Lista
-        pets={[
-          {
-            id: 1, 
-            nome: 'Bidu',
-            historia:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eos quam? Aliquam eum recusandae id eligendi iste doloribus nobis et consequatur neque. Voluptatem optio fugiat ut enim suscipit repellat amet.",
-            foto: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png"
-          },
-          {
-            id: 2, 
-            nome: 'Flokinho',
-            historia:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eos quam? Aliquam eum recusandae id eligendi iste doloribus nobis et consequatur neque. Voluptatem optio fugiat ut enim suscipit repellat amet.",
-            foto: "https://www.petlove.com.br/dicas/wp-content/uploads/2020/10/Cachorro-culpa-Petlove.jpg"
-          }
-          ,
-          {
-            id: 3, 
-            nome: 'Belinha',
-            historia:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eos quam? Aliquam eum recusandae id eligendi iste doloribus nobis et consequatur neque. Voluptatem optio fugiat ut enim suscipit repellat amet.",
-            foto: "https://blogs.correiobraziliense.com.br/nqv/wp-content/uploads/sites/22/2018/08/DjubZ3nXgAAw4e--1280x720.jpg"
-          }
-          ,
-          {
-            id: 4, 
-            nome: 'Ursinho',
-            historia:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eos quam? Aliquam eum recusandae id eligendi iste doloribus nobis et consequatur neque. Voluptatem optio fugiat ut enim suscipit repellat amet.",
-            foto: "https://meupet.elanco.com/sites/g/files/adhwdz661/files/styles/paragraph_image/public/2020-07/gato_de_retrato_olhando_para_a_camera.jpg?itok=iXWiqVkY"
-          }
-          
-        ]} 
+        pets={listaPets} 
+        onSelect={(pet) => setPetSelecionado(pet)}
+      />
+
+      <Dialog 
+        open={petSelecionado !== null} 
+        fullWidth
+        PaperProps={{ sx: { p: 5}}}
+        onClose={() => setPetSelecionado(null)}
+      >
+        <Grid container spacing={2} >
+          <Grid item xs={12} >
+            <TextField 
+              label={'E-mail'}
+              type={'email'}
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} >
+            <TextField 
+              label={'Quantia por mês'}
+              type={'number'}
+              fullWidth
+              value={valor}
+              onChange={(e) => setValor(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+        <DialogActions sx={{mt: 5}} >
+          <Button 
+          color={'secondary'}
+          onClick={() => setPetSelecionado(null)}
+          >
+            Cancelar
+          </Button>
+          <Button 
+            variant={'contained'}
+            onClick={() => adotar()}
+          >
+            Confirmar adoção
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Snackbar 
+        open={mensagem.length > 0}
+        message={mensagem}
+        autoHideDuration={2500}
+        onClose={() => setMensagem('')}
       />
     </div>
   )
